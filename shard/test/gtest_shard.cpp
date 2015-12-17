5b1cd28ea4c66282816296c8e103e8c1dc7b6002
Josh Chen <bochfchen.test@gmail.com>
2015-12-16 23:12:19 -0500

    balance

--- shard/test/gtest_shard.cpp
+++ shard/test/gtest_shard.cpp
@@ -4,9 +4,11 @@
 #include <tuple>
 #include <cstdint>
 #include <chrono>
+#include <cmath>
 
 #include <gtest/gtest.h>
 #include <shard_mod.h>
+#include <shard_jch.h>
 
 using namespace std;
 using namespace ::testing;
@@ -22,7 +24,35 @@ typedef tuple<int, int, bool> ShardParams;
 class TestShard : public TestWithParam<ShardParams>
 {
   public:
-    static void run(IShard * shard, const ShardParams& params)
+		static void balance(IShard * shard, const ShardParams& params) {
+			const size_t n = 1 << get<0>(params); // number of elements
+			const size_t bucket = get<1>(params); // initial buckets
+
+			ASSERT_FALSE(shard == NULL);
+			ASSERT_GT(n, 1000);
+			ASSERT_LT(n, 20000000);  // total elements to be tested between (1000, 20M)
+			ASSERT_GT(bucket, 10);
+			ASSERT_LT(bucket, 1000000);  // total buckets between (10, 1M)
+
+			// initialize buckets
+			vector<size_t> buckets;
+			for (size_t i = 0; i < bucket; ++i) {
+				buckets.push_back(0);
+			}
+
+			// calcuate bucket number for each element
+			for (size_t key = 0; key < n; ++key) {
+				buckets[shard->hash(key, bucket)]++;
+			}
+
+			// get standard deviation as balance ratio
+			double avg = average(buckets);
+			double balance = deviation(buckets, avg);
+
+			cout << n << ", " << bucket << ", " << avg << ", " << balance << endl;
+		}
+
+    static void migration(IShard * shard, const ShardParams& params)
     {
 			const size_t n = 1 << get<0>(params); // number of elements
 			const size_t bucket1 = get<1>(params); // initial buckets
@@ -59,12 +89,46 @@ class TestShard : public TestWithParam<ShardParams>
 			chrono::nanoseconds ns = chrono::duration_cast<chrono::nanoseconds>(finish - start);
 			cout << n << ", " << bucket1 << ", " << bucket2 << ", " << moved << ", " << ns.count() << endl;
     }
+
+	private:
+		static double average(const vector<size_t>& v) {
+			double sum = 0.0;
+			for (size_t i = 0; i < v.size(); ++i) {
+				sum += v[i];
+			}
+			return sum/v.size();
+		}
+
+		static double deviation(const vector<size_t>& v, const double avg) {
+			double E = 0;
+			for (size_t i = 0; i < v.size(); ++i) {
+				E += (v[i] - avg) * (v[i] - avg);
+			}
+			return sqrt(E/v.size());
+		}
 };
 
 TEST_P(TestShard, Mod)
 {
 	ShardMod shard;
-  run(&shard, GetParam());
+  migration(&shard, GetParam());
+}
+
+TEST_P(TestShard, JCH)
+{
+	ShardJumpConsistentHash shard;
+  migration(&shard, GetParam());
+}
+
+TEST_P(TestShard, ModBalance) {
+	ShardMod shard;
+  balance(&shard, GetParam());
+}
+
+TEST_P(TestShard, JCHBalance)
+{
+	ShardJumpConsistentHash shard;
+  balance(&shard, GetParam());
 }
 
 INSTANTIATE_TEST_CASE_P(Sharding,
