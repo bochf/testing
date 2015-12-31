7ad3cd536de16cac7e09ea04edd28b47b5ae950b
Josh Chen <bochfchen.code@gmail.com>
2015-12-31 19:51:16 +0000

    add mixed

---
+++ practice/mixed/README.md
@@ -0,0 +1,2 @@
+# compile c and cpp source code
+## using extern "C" {} to declare functions if it will be used in c++
---
+++ practice/mixed/foo.h
@@ -0,0 +1,13 @@
+#ifndef __FOO_H__
+#define __FOO_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void foo();
+
+#ifdef __cplusplus
+}
+#endif
+#endif
