840f2adef4369c5c6394880acdabd9964561237b
Josh Chen <bochfchen.code@gmail.com>
2016-09-30 16:08:43 +0000

    add more cases

--- practice/smalltest/test_autoptr.cpp
+++ practice/smalltest/test_autoptr.cpp
@@ -3,36 +3,40 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-class Foo
+class MockFoo
 {
-	public:
-		Foo()
-		{
-		}
+  public:
+    virtual ~MockFoo() {
+      destroy();
+    }
 
-		virtual ~Foo()
-		{
-		}
-
-		// void construct() = 0;
-		// void destroy() = 0;
-};
-
-class MockFoo // : public Foo
-{
-	public:
-		virtual ~MockFoo()
-		{
-			destroy();
-		}
-
-		MOCK_METHOD0(construct, void());
-		MOCK_METHOD0(destroy, void());
+    MOCK_METHOD0(destroy, void());
+    MOCK_METHOD0(test, void());
 };
 
 TEST(TestAutoPtr, create)
 {
-	MockFoo * pMockFoo = new MockFoo();
-	AutoPtr<MockFoo> apMockFoo(pMockFoo);
-	EXPECT_CALL(*pMockFoo, destroy()).Times(1);
+  MockFoo * pMockFoo = new MockFoo();
+  AutoPtr<MockFoo> apMockFoo(pMockFoo);
+  EXPECT_CALL(*pMockFoo, destroy()).Times(1);
+}
+
+TEST(TestAutoPtr, set)
+{
+  AutoPtr<MockFoo> apMockFoo;
+  MockFoo * pMockFoo = new MockFoo();
+  apMockFoo.set(pMockFoo);
+  EXPECT_CALL(*pMockFoo, destroy()).Times(1);
+}
+
+TEST(TestAutoPtr, release)
+{
+  MockFoo * pMockFoo = NULL;
+  {
+    AutoPtr<MockFoo> apMockFoo(new MockFoo());
+    pMockFoo = apMockFoo.release();
+    EXPECT_CALL(*pMockFoo, destroy()).Times(0);
+  }
+  EXPECT_CALL(*pMockFoo, destroy()).Times(1);
+  delete pMockFoo;
 }
